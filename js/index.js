d831cbc74247ecc0d5964d2a225c6d7508787828
DakotaWilliams13 <zlata.ivanova.13.03@gmail.com>
2026-06-01 11:00:12 +0500

    ..

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const menu = document.querySelector('.menu');
+const menu = document.querySelector('.dropdown');
 const burgerBtn = document.querySelector('.burger');
 
 burgerBtn.addEventListener('click', () => {
